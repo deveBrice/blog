a8cae459445d8f912f5e32cf8eabf45fabe6005e
deveBrice <brice.william971@gmail.com>
2021-12-01 11:25:37 +0100

    fill of articles list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,20 +9,33 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import { BrowserComponent } from '../shared/browser/browser.component';
+import { HomeComponent } from './home/home.component';
+import { ArticlesListComponent } from './articles-list/articles-list.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrowserComponent
+    BrowserComponent,
+    HomeComponent,
+    ArticlesListComponent,
+    ArticleDetailsComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/article-details/article-details.component.ts
+++ src/app/article-details/article-details.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-article-details',
   templateUrl: './article-details.component.html',
   styleUrls: ['./article-details.component.scss']
 })
-export class ArticleDetailsComponent implements OnInit {
 
-  constructor() { }
+export class ArticleDetailsComponent implements OnInit {
+   commentsForm: FormGroup
+    
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+     this.comment()
   }
 
+  comments = [
+    { id : 1, post_id : 1, content : "..." },
+    { id : 2, post_id : 2, content : "..." },
+    { id : 3, post_id : 2, content : "..." },
+    { id : 4, post_id : 2, content : "..." },
+  ];
+
+  comment = () => {
+    this.commentsForm = this.fb.group({
+        text: ['']
+    })
+  }
+
+  addComment = () => {
+    this.commentsForm.valueChanges.subscribe(
+      res => {
+        this.comments.push({ id : 1, post_id : 1, content : res.text })
+      }
+    )
+  }
 }
